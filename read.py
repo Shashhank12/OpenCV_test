e86f1e681f65d1a0df8dd0c509ee3088f6b50884
Shashhank12 <59421940+Shashhank12@users.noreply.github.com>
2022-01-29 13:37:17 -0800

    fourth commit

--- read.py
+++ read.py
@@ -9,15 +9,24 @@ import cv2 as cv #imports opencv (cv2) called cv
 #Normally takes an int. Int represents a camera for input(0 is for webcam, 1 is camera plugged in, etc)
 #Can also take a path file for a video
 
-capture = cv.VideoCapture('Videos/Buildings.gif')
+#initializes the variable capture with a file path
+# capture = cv.VideoCapture('Videos\Buildings.gif')
+capture = cv.VideoCapture('Videos\Test.mp4')
 
+#While loop to show frame by frame
 while True:
+    #reads frame by frame
     isTrue, frame = capture.read()
+    #shows frame by frame
+    cv.imshow('Video', frame)
 
+    #If waitKey delay is 20 miliseconds and d key is pressed, video ends
     if cv.waitKey(20) & 0xFF==ord('d'):
         break
 
+#realeases the path from capture
 capture.release()
+#destroys all the windows
 cv.destroyAllWindows()
 
 #Waits an infinite amount of time for a key press
