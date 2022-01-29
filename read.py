6e28e758d2173367a7f6e1dd2d5be4a8fdcbb3b1
Shashhank12 <59421940+Shashhank12@users.noreply.github.com>
2022-01-29 13:11:26 -0800

    third commit

--- read.py
+++ read.py
@@ -1,7 +1,24 @@
 import cv2 as cv #imports opencv (cv2) called cv
 
-img = cv.imread('Photos/Cyberpunk.jpg') 
+#For image
+# img = cv.imread('Photos/Cyberpunk.jpg') 
 
-cv.imshow('City', img)
+#For showing image
+# cv.imshow('City', img)
 
-cv.waitKey(0)
+#Normally takes an int. Int represents a camera for input(0 is for webcam, 1 is camera plugged in, etc)
+#Can also take a path file for a video
+
+capture = cv.VideoCapture('Videos/Buildings.gif')
+
+while True:
+    isTrue, frame = capture.read()
+
+    if cv.waitKey(20) & 0xFF==ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
+
+#Waits an infinite amount of time for a key press
+# cv.waitKey(0)
